58789dd902afe055495030f8af4b97f8dc513395
Matt Yamout <matthew.yamout@gmail.com>
2016-09-05 01:10:32 -0700

    Added javascript file and added minor functionality

--- llamaglama.js
+++ llamaglama.js
@@ -1,9 +1,19 @@
 (() => {
+
 	const app = angular.module('LlamaGlama', []);
 
 	app.controller('LlamaGame', ['$scope', function($scope) {
-		$scope.test = () => {
-			alert("Test work!!!");
+		$scope.llamaName = '';
+		$scope.personality = '';
+		$scope.personalities = ['Athlete', 'Artist', 'Adventurer', 'Teenager', 'Comedian', 'Engineer'];
+
+		$scope.captureName = () => {
+			console.log($scope.llamaName);
+			console.log($scope.personality);
+		};
+		$scope.clear = () => {
+			$scope.llamaName = '';
+			$scope.personality = '';
 		};
 	}]);
 
