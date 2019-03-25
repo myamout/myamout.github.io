61d10ad3de3aff42885998a4874a4a070af1785c
Matthew Yamout <myamout@Matthews-MacBook-Pro.local>
2019-03-24 20:25:51 -0400

    added start of website skeleton

---
+++ js/index.js
@@ -0,0 +1,4 @@
+window.onload = setTimeout(function () {
+    document.getElementById("spinner").setAttribute("hidden", "");
+    document.getElementById("mainContent").removeAttribute("hidden");
+}, 3000);
